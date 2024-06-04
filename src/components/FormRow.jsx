5648c690ac59d8183b0740687292829753adf66d
Pablo Sangenis <sangenispablo@gmail.com>
2024-06-03 22:10:43 -0300

    Register listo

---
+++ src/components/FormRow.jsx
@@ -0,0 +1,17 @@
+export default function FormRow({ type, name, labelText, defaultValue = "" }) {
+  return (
+    <div className="form-row">
+      <label htmlFor={name} className="form-label">
+        {labelText || name}
+      </label>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        className="form-input"
+        defaultValue={defaultValue}
+        required
+      />
+    </div>
+  );
+}
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,26 @@
 import { Link } from "react-router-dom";
 
-import { Logo } from "../components";
+import { FormRow, Logo } from "../components";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 
 export default function Register() {
   return (
-    <div>
-      <h1>Register</h1>
-      <Link to={"/login"}>Login</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type="text" name="name" labelText="name" defaultValue="john"/>
+        <FormRow type="text" name="lastName" labelText="last name" defaultValue="smith"/>
+        <FormRow type="text" name="location" defaultValue="earth"/>
+        <FormRow type="email" name="email" defaultValue="john@gmail.com"/>
+        <FormRow type="password" name="password" defaultValue="pepa123"/>
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+        <p>
+          Already member ? <Link to={"/login"}>Login</Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 }
